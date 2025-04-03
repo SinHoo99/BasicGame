df79b29e1a55537016465325d3e4741cf28c77f4
SinHoo99 <tjdqls991218@gmail.com>
2025-04-03 18:59:25 +0900

    feat: 장애물 오브젝트 풀 반환 기능 추가
    - 카메라 밖으로 벗어난 장애물을 자동으로 SetActive(false) 처리

--- Assets/02.Scripts/PoolObject/Obstacle.cs
+++ Assets/02.Scripts/PoolObject/Obstacle.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class Obstacle : PoolObject
 {
-    
+    private bool wasVisible = false;
+
+    void OnBecameVisible()
+    {
+        wasVisible = true;
+    }
+
+    void OnBecameInvisible()
+    {
+        if (wasVisible)
+        {
+            gameObject.SetActive(false);
+            wasVisible = false; // 다음 활성화를 위해 리셋
+        }
+    }
 }
