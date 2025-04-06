a0de00ec9104ba3fb28f0169ab8b5f38c385ca1e
SinHoo99 <tjdqls991218@gmail.com>
2025-04-06 22:25:16 +0900

    fix : 태그 위치 수정
    태그 위치가 잘못 되어 콜라이더 인식을 못하여 위치 수정함

--- Assets/02.Scripts/Test/ObstacleCollider.cs
+++ Assets/02.Scripts/Test/ObstacleCollider.cs
@@ -1,23 +1,18 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class ObstacleCollider : MonoBehaviour
 {
-
     private bool hasScored = false;
-
-    private void OnTriggerEnter(Collider other)
+    private void OnEnable()
     {
-        if (other.CompareTag("Player") && !hasScored)
+        hasScored = false;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !hasScored)
         {
             Debug.Log("HI");
             hasScored = true;
         }
     }
-
-    private void OnEnable()
-    {
-        hasScored = false; 
-    }
-
 }
