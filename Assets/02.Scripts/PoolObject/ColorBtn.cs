b02a8c7377b447a158815526f11314c8fb792a2e
SinHoo99 <tjdqls991218@gmail.com>
2025-04-08 21:48:09 +0900

    Add : 버튼정보수집
    - 버튼 누르면 현재 버튼의 정보를 수집하는 기능 추가

--- Assets/02.Scripts/PoolObject/ColorBtn.cs
+++ Assets/02.Scripts/PoolObject/ColorBtn.cs
@@ -7,9 +7,23 @@ public class ColorBtn : MonoBehaviour
     public Image colorImage;
     public TextMeshProUGUI nameText;
 
+    private ColorData currentData;
     public void Setup(ColorData data)
     {
+        currentData = data;
+
         colorImage.color = data.GetUnityColor();
         nameText.text = data.Name;
+    }
+
+    public void SetColor()
+    {
+        if (currentData == null)
+        {
+            Debug.LogWarning("ColorData가 설정되지 않았습니다.");
+            return;
+        }
+
+        Debug.Log($"버튼 클릭됨: ID: {(int)currentData.ID}, 이름: {currentData.Name}, RGB: ({currentData.R}, {currentData.G}, {currentData.B})");
     }
 }
