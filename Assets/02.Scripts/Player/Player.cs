dbebdb39bc0de84bc6a0bf30fcdf90dc73d28624
SinHoo99 <tjdqls991218@gmail.com>
2025-04-08 17:46:08 +0900

    Test : 플레이어 색깔과 파티클색깔 동기화 테스트
    - 플레이어의 색과 파티클의 색깔 변경 테스트
    - SO파일을 이용하여 참조문제 및 결합도문제 해결

--- Assets/02.Scripts/Player/Player.cs
+++ Assets/02.Scripts/Player/Player.cs
@@ -7,8 +7,11 @@ public class Player : MonoBehaviour
     [SerializeField] private PlayerMovement movement;
 
     private GameObject PlayerSO => GameManager.Instance.GetPlayerSOData().PlayerPrefab;
-    private Color playerColor;
+    private SpriteRenderer spriteRenderer;
     private ParticleSystem playerParticleSystem;
+
+    private float timer = 0f;
+    private float interval = 1f;
 
     private void Awake()
     {
@@ -16,16 +19,32 @@ public class Player : MonoBehaviour
         input.OnDragMove += indicator.OnDragMove;
         input.OnDragEnd += indicator.OnDragEnd;
         input.OnDragEnd += movement.OnDragEnd;
-    }
 
-    private void Start()
-    {
-        playerColor = PlayerSO.GetComponentInChildren<Color>();
-        playerParticleSystem = PlayerSO.GetComponentInChildren<ParticleSystem>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        playerParticleSystem = GetComponentInChildren<ParticleSystem>();
     }
 
     private void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        if (timer >= interval)
+        {
+            timer = 0f;
+
+            // 랜덤 색상 생성
+            Color randomColor = new Color(Random.value, Random.value, Random.value);
+
+            // SpriteRenderer에 적용
+            if (spriteRenderer != null)
+                spriteRenderer.color = randomColor;
+
+            // ParticleSystem에 적용
+            if (playerParticleSystem != null)
+            {
+                var main = playerParticleSystem.main;
+                main.startColor = randomColor;
+            }
+        }
     }
 }
