f38b680184f65b3f926e6f06a3848e2657180867
SinHoo99 <tjdqls991218@gmail.com>
2025-04-11 18:56:49 +0900

    Add : 씬 옮길시 세팅팝업 비활성화 기능 추가

--- Assets/02.Scripts/Test/Test.cs
+++ Assets/02.Scripts/Test/Test.cs
@@ -3,19 +3,25 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-
+    [SerializeField] private GameObject SettingPopUp;
     public void GoMainScene()
     {
-       SceneLoadManager.LoadScene(2);
+        SceneLoadManager.LoadScene(2);
+        if (SettingPopUp != null && SettingPopUp.activeSelf)
+        {
+            SettingPopUp.SetActive(false);
+        }
     }
 
     public void GoStartScene()
     {
         SceneLoadManager.LoadScene(0);
+        SettingPopUp.SetActive(false);
     }
 
     public void GoStoreScene()
     {
         SceneLoadManager.LoadScene(3);
+        SettingPopUp.SetActive(false);
     }
 }
