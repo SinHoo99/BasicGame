75fe057d07602468f67354271cb9882856bfb7f9
SinHoo99 <tjdqls991218@gmail.com>
2025-04-16 16:44:51 +0900

    Add : Interface 추가
    - 각 장애물의 변동성을 주기위한 인터페이스 추가

--- Assets/02.Scripts/PoolObject/Obstacle.cs
+++ Assets/02.Scripts/PoolObject/Obstacle.cs
@@ -1,62 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class Obstacle : PoolObject
 {
-   /* private Dictionary<int, Color> scoreColorMap = new Dictionary<int, Color>()
-    {
-        {10, Color.cyan },
-        {20, Color.red },
-        {30, Color.blue },
-        {40, Color.magenta }
-    };*/
-
     private bool wasVisible = false;
-
-    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rb;
+    [SerializeField] private float speed = 1f;
 
     private void Awake()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
     }
+
     private void OnEnable()
     {
-       /* UpdateColorByScore(GameManager.Instance.GetNextObstacleIndex());*/
+        rb.velocity = Vector2.right * speed;
     }
-    void OnBecameVisible()
+
+    private void OnDisable()
+    {
+        wasVisible = false;
+        rb.velocity = Vector2.zero;
+    }
+
+    private void OnBecameVisible()
     {
         wasVisible = true;
     }
 
-    void OnBecameInvisible()
+    private void OnBecameInvisible()
     {
         if (wasVisible)
-        {
             gameObject.SetActive(false);
-            wasVisible = false; // 다음 활성화를 위해 리셋
-        }
     }
-
- /*   private void UpdateColorByScore(int score)
-    {
-        Color resultColor = Color.white;
-
-        foreach (var kvp in scoreColorMap)
-        {
-            if(score < kvp.Key)
-            {
-                resultColor = kvp.Value;
-                break;
-            }
-        }
-
-        if(score >= scoreColorMap.Keys.Max())
-        {
-            resultColor = scoreColorMap[scoreColorMap.Keys.Max()];
-        }
-
-        spriteRenderer.color = resultColor; 
-    }*/
 }
