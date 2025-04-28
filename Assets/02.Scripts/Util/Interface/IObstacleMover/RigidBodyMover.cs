01e5b7925ba558a4299157109d997cdd6bc5d544
SinHoo99 <tjdqls991218@gmail.com>
2025-04-28 16:16:03 +0900

    Add : Obstacle 애니메이션 추가

--- Assets/02.Scripts/Util/Interface/IObstacleMover/RigidBodyMover.cs
+++ Assets/02.Scripts/Util/Interface/IObstacleMover/RigidBodyMover.cs
@@ -3,30 +3,35 @@ using UnityEngine;
 public class RigidbodyMover : IObstacleMover
 {
     private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
 
-    // 원하는 속도 범위 설정
     private float minSpeed = 0.5f;
     private float maxSpeed = 1f;
 
-    public void Move(Transform target)
+    public void Init(Transform target)
     {
         rb = target.GetComponent<Rigidbody2D>();
+        spriteRenderer = target.GetComponent<SpriteRenderer>();
+    }
+
+    public void Move(Transform target)
+    {
+        if (rb == null || spriteRenderer == null)
+            Init(target);
 
         float cameraCenterX = Camera.main.transform.position.x;
 
-        // 위치에 따라 방향 결정
         Vector2 direction = target.position.x < cameraCenterX
-            ? (Vector2.right).normalized
-            : (Vector2.left).normalized;
+            ? Vector2.right
+            : Vector2.left;
 
         float randomSpeed = Random.Range(minSpeed, maxSpeed);
 
         rb.velocity = direction * randomSpeed;
 
-        // 스프라이트 방향 조정
-        target.localScale = new Vector3(direction.x > 0 ? 1 : -1, 1, 1);
+        if (spriteRenderer != null)
+            spriteRenderer.flipX = direction.x < 0;
     }
-
 
     public void Stop()
     {
